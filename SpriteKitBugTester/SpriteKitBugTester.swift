48375a39d6522c5994d513542bb4a3ddd34a5cc7
Daniel Kanaan <dgk@danielkanaan.com>
2016-07-01 13:14:05 -0700

    Working on 10.0 part 1

--- SpriteKitBugTester/SpriteKitBugTester.swift
+++ SpriteKitBugTester/SpriteKitBugTester.swift
@@ -31,7 +31,7 @@ public class SpriteKitBugTester {
             return sharedInstance.spriteKitBugPresent
         }
     }
-    class func complete (bugPresent bugPresent:Bool) {
+    class func complete (bugPresent:Bool) {
         sharedInstance.spriteKitBugTestComplete = true
         sharedInstance.spriteKitBugPresent = bugPresent
         sharedInstance.completion?()
@@ -41,11 +41,11 @@ public class SpriteKitBugTester {
         //SpriteKit Bug
         var bugPresent:Bool = true
         var bugTestInProgress:Bool = true
-        var bugTimer:NSTimer?
-        var blockTimer:NSTimer?
+        var bugTimer:Timer?
+        var blockTimer:Timer?
         public var launchImage:String?
         
-        public override func didMoveToView(view: SKView) {
+        public override func didMove(to view: SKView) {
             if launchImage != nil {
                 let background = SKSpriteNode(imageNamed: launchImage!)
                 background.size = self.size
@@ -55,25 +55,25 @@ public class SpriteKitBugTester {
             }
             let bugTest1 = SKNode()
             bugTest1.name = "Bug Test 1"
-            bugTest1.position = CGPointZero
+            bugTest1.position = CGPoint.zero
             addChild(bugTest1)
             let bugTest2 = SKNode()
             bugTest2.name = "Bug Test 2"
-            bugTest2.position = CGPointZero
+            bugTest2.position = CGPoint.zero
             bugTest1.addChild(bugTest2)
-            let wait:NSTimeInterval = 0.003
-            func removeAndMove(card:SKNode) {
+            let wait:TimeInterval = 0.003
+            func removeAndMove(_ card:SKNode) {
                 let oldParent = card.parent
                 card.removeFromParent()
                 addChild(card)
-                self.runAction(SKAction.waitForDuration(wait)){
+                self.run(SKAction.wait(forDuration: wait)){
                     card.removeFromParent()
                     oldParent!.addChild(card)
                     self.bugTestInProgress = false
                 }
             }
             removeAndMove(bugTest2)
-            bugTimer = NSTimer.scheduledTimerWithTimeInterval(0.001, target: self, selector: #selector(SpriteKitBugScene.bugTest2), userInfo: nil, repeats: true)
+            bugTimer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(SpriteKitBugScene.bugTest2), userInfo: nil, repeats: true)
         }
         
         func bugTest2 () {
@@ -81,8 +81,8 @@ public class SpriteKitBugTester {
                 return
             }
             bugTimer?.invalidate()
-            let bugTest1 = childNodeWithName("Bug Test 1")!
-            let bugTest2 = bugTest1.childNodeWithName("Bug Test 2")!
+            let bugTest1 = childNode(withName: "Bug Test 1")!
+            let bugTest2 = bugTest1.childNode(withName: "Bug Test 2")!
             if bugTest2.parent != nil {
                 self.bugPresent = false
                 NSLog("Bug not present")
